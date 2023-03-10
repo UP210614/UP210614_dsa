e8a50b43c431e4332774de7beb879d5d7368dc0b
UP210614 <up210614@alumnos.upa.edu.mx>
2023-03-10 09:30:03 -0600

    calculadora

--- U2/calculadora.py
+++ U2/calculadora.py
@@ -1,4 +1,6 @@
-from collections import deque
+import math
+operadores = ['+', '-', '/', '*', '(', ')', '^','sen','tan','cos','asin','acos','atan','ln','log']
+funciones = ['sen','tan','cos','asen','acos','atan','ln','log']
 def prioridad(c):
     if c in ['+','-']:
         return 1
@@ -6,6 +8,8 @@ def prioridad(c):
         return 2
     elif c == '^':
         return 3
+    elif c in funciones:
+        return 4
     elif c in ['(',')']:
         return 0
 def infix_posfix(n):
@@ -14,11 +18,11 @@ def infix_posfix(n):
     p = []
     stack = []
     for i in n:
-        if i not in ['+', '-', '/', '*', '(', ')', '^']:
+        if i not in ['+', '-', '/', '*', '(', ')', '^','sen','tan','cos','asen','acos','atan','ln','log']:
             p.append(i)
         elif i == '(':
             stack.append(i)
-        elif i in ['+', '-', '/', '*','^']:
+        elif i in ['+', '-', '/', '*','^','sen','tan','cos','asen','acos','atan','ln','log']:
             contador = len(stack)-1
             while prioridad(stack[contador]) >= prioridad(i):
                 p.append(stack.pop())
@@ -33,9 +37,13 @@ def infix_posfix(n):
     p.append(')')
     return p
 def operacion(operacion):
+    # if operacion[1] in ['+','-']:
+    #     a = operacion[1]+operacion[0]
+    #     operacion[0] = a
+    #     del operacion[1]
     stack = []
     for i in operacion:
-        if i not in ['+','/','-','*','^',')','^']:
+        if i not in ['+','/','-','*','^',')','^','sen','tan','cos','asen','acos','atan','ln','log']:
             numero = float(i)
             stack.append(numero)
         else:
@@ -65,6 +73,31 @@ def operacion(operacion):
                     a = stack.pop()
                     c = a**b
                     stack.append(c)
+                case 'sen':
+                    x = math.sin(math.radians(stack.pop()))
+                    stack.append(x)
+                case 'cos':
+                    x = math.cos(math.radians(stack.pop()))
+                    stack.append(x)
+                case 'tan':
+                    x = math.tan(math.radians(stack.pop()))
+                    stack.append(x)
+                case 'asen':
+                    x = math.degrees(math.asin(stack.pop()))
+                    stack.append(x)
+                case 'acos':
+                    x = math.degrees(math.acos(stack.pop()))
+                    stack.append(x)
+                case 'atan':
+                    x = math.degrees(math.atan(stack.pop()))
+                    stack.append(x)
+                case 'ln':
+                    x = stack.pop()
+                    stack.append(math.log(x))
+                case 'log':
+                    x = stack.pop()
+                    z = stack.pop()
+                    stack.append(math.log(x,z))
                 case ')':
                     break
                 case _:
@@ -82,6 +115,9 @@ def enlistar(n):
                 op.append(a)
             op.append(i)
             a = ''
+    if a != '':
+        op.append(a)
+
     return op
 def operacionFinal(op):
     a = enlistar(op)
@@ -91,9 +127,5 @@ def operacionFinal(op):
 
 n = input("Introduzca una operación: ")
 
-#a = enlistar(n)
-#b = infix_posfix(a)
-#c = operacion(b)
 print(operacionFinal(n))
-
 
