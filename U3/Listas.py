349babf860f374214bade6d8b937db0c03d15c64
UP210614 <up210614@alumnos.upa.edu.mx>
2023-04-04 22:14:07 -0600

    Listas final

--- U3/Listas.py
+++ U3/Listas.py
@@ -22,64 +22,121 @@ class Lista:
         if self.isEmpty():
             self.head = newNode
             self.tail = newNode
+            self.size +=1
+            return pos
         elif dato <= self.head.data:
             newNode.next = self.head
             self.head.prev = newNode
             self.head = newNode
+            self.size +=1
+            return pos
         else:
             dir = self.head
-
-            # while dir.next != None:
-            #     if dato <= dir.data:
-            #         newNode.prev = dir.prev
-            #         newNode.next = dir
-            #         dir.prev.next = newNode
-            #         dir.prev = newNode
-            #         self.size += 1
-            #         return
-            #     dir = dir.next
-            #     a = dir.data
-            while True:
-                if dato <= dir.data:
-                    newNode.prev = dir.prev
-                    newNode.next = dir
-                    dir.prev.next = newNode
-                    dir.prev = newNode
-                    self.size += 1
-                    return
-                elif dir.next == None:
-                    break
+            pos = 0
+            while dir.next is not None and dir.next.data <= dato:
                 dir = dir.next
+                pos += 1
             newNode.prev = dir
+            newNode.next = dir.next
+            if dir.next is not None:
+                dir.next.prev = newNode
+            else:
+                self.tail = newNode
             dir.next = newNode
-            self.tail = newNode
-        self.size +=1
+            self.size += 1
+            pos += 1
+            return pos
+
     def recorrerDerecha(self):
-        lista = []
         dato = self.head
+        lista = ""
         for i in range(self.size):
-            lista.append(dato.data)
+            if i < self.size - 1:
+                lista += str(dato.data) + " --> "
+            else:
+                lista += str(dato.data)
             dato = dato.next
         return lista
     def recorrerIzquierda(self):
-        lista = []
         dato = self.tail
+        lista = ""
         for i in range(self.size):
-            lista.append(dato.data)
+            if i < self.size - 1:
+                lista += str(dato.data) + " --> "
+            else:
+                lista += str(dato.data)
             dato = dato.prev
         return lista
+    def seek(self,pos):
+        if pos < self.size and self.isEmpty() is False and pos >= 0:
+            dir = self.head
+            for i in range(pos):
+                dir = dir.next
+            return dir.data
+        else:
+            return None
+    def search(self,value):
+        if self.isEmpty() is False:
+            dir = self.head
+            for i in range(self.size):
+                if dir.data == value:
+                    return i
+                dir = dir.next
+            return None
+        else:
+            return None
+    def removePosition(self,pos):
+        if pos < self.size and self.isEmpty() is False:
+            dir = self.head
+            if pos == 0:
+                eliminar = dir
+                dato = dir.data
+                dir.next.prev = None
+                self.head = dir.next
+                self.size-=1
+                del eliminar
+                return dato
+            else:
+                for i in range(pos):
+                    dir = dir.next
+                eliminar = dir
+                dato = dir.data
+                if dir.next != None:
+                    dir.prev.next = dir.next
+                    dir.next.prev = dir.prev
+                else:
+                    dir.prev.next = None
+                    self.tail = dir.prev
+                del eliminar
+                self.size-=1
+                return dato
+        else:
+            return None
+    def removeValue(self,value):
+        if self.isEmpty() is False:
+            dir = self.head
+            if value == self.head.data:
+                eliminar = dir
+                dato = dir.data
+                dir.next.prev = None
+                self.head = dir.next
+                self.size -= 1
+                del eliminar
+                return dato
+            else:
+                while value != dir.data:
+                    dir = dir.next
+                eliminar = dir
+                dato = dir.data
+                if dir.next != None:
+                    dir.prev.next = dir.next
+                    dir.next.prev = dir.prev
+                else:
+                    dir.prev.next = None
+                    self.tail = dir.prev
+                del eliminar
+                self.size -= 1
+                return dato
+        else:
+            return None
 
-a = Lista()
-a.insert(4)
-a.insert(3)
-a.insert(2)
-a.insert(1)
-a.insert(5)
-a.insert(4.5)
-a.insert(6)
-a.insert(4)
-a.insert(4.6)
-a.insert(5.5)
-
-print(a.recorrerDerecha())
-print(a.recorrerIzquierda())
